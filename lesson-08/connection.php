9be6d0df9938f85032420021712eee3601dac774
bones101 <sbone27@comcast.net>
2013-03-17 12:00:13 -0700

    Update connection.php
    Took out my password

--- lesson-08/connection.php
+++ lesson-08/connection.php
@@ -1,3 +1,3 @@
 <?php
-$dataconnection = new MySQLi("localhost","sbone1_admin","MpiZ11Mm", "sbone1" );
+$dataconnection = new MySQLi("localhost","sbone1_admin","(my password)", "sbone1" );
 /*Don't close php with ?>*/
